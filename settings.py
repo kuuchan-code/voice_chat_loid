d6d61e7309fb20d5b427bd62efe322a88ad51148
kuuchan-code <kuuchanxn@gmail.com>
2023-12-31 03:48:46 +0900

    update

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ SPEAKERS_URL = "http://127.0.0.1:50021/speakers"
 AUDIO_QUERY_URL = "http://127.0.0.1:50021/audio_query"
 SYNTHESIS_URL = "http://127.0.0.1:50021/synthesis"
 STYLE_SETTINGS_FILE = "style_settings.json"
-TEST_GUILD_ID = discord.Object(id="1129051248574869617")
+TEST_GUILD_ID = discord.Object(id="1189256965172514836")
 
 
 # gender_categories = {
