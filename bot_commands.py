0b8cfb5b163192d0f912301b90326453a3ea80d4
kuuchan-code <kuuchanxn@gmail.com>
2023-12-31 14:51:10 +0900

    update

--- bot_commands.py
+++ bot_commands.py
@@ -1,20 +1,35 @@
 import asyncio
 import discord
 from settings import (
+    CHARACTORS_INFO,
+    SPEAKERS_ENDPOINT,
     TEST_GUILD_ID,
     USER_DEFAULT_STYLE_ID,
     NOTIFY_DEFAULT_STYLE_ID,
 )
+from voicevox_client import fetch_json
 
 def setup_commands(bot):
+    speakers_data = fetch_json(SPEAKERS_ENDPOINT)
+
     @bot.tree.command(
-        name="leave", guild=TEST_GUILD_ID, description="ボットをボイスチャンネルから切断します。"
+        name="list", guild=TEST_GUILD_ID, description="スピーカーとそのスタイルIDを表示します。"
     )
-    async def leave(interaction: discord.Interaction):
-        if interaction.guild.voice_client:
-            guild_id = str(interaction.guild_id)
-            await clear_playback_queue(guild_id)  # キューをクリア
-            if "text_channel" in speaker_settings.get(guild_id, {}):
-                del speaker_settings[guild_id]["text_channel"]
-            await interaction.guild.voice_client.disconnect()  # 切断
-            await interaction.response.send_message("ボイスチャンネルから切断しました。")
+    async def list(interaction: discord.Interaction):
+        """スピーカーとそのスタイルIDを表示します。"""
+        if not speakers_data:
+            await interaction.response.send_message("スピーカーのデータを取得できませんでした。")
+            return
+
+        # メッセージを整形して作成
+        message = "**利用可能なスピーカーとスタイル:**\n"
+        for speaker in speakers_data:
+            name = speaker["name"]
+            character_id = CHARACTORS_INFO.get(name, "unknown")  # キャラクターIDを取得
+            url = f"https://voicevox.hiroshiba.jp/dormitory/{character_id}/"
+            styles = ", ".join(
+                [f"{style['name']} (ID: {style['id']})" for style in speaker["styles"]]
+            )
+            message += f"\n[{name}]({url}): {styles}"
+
+        await interaction.response.send_message(message)
--- run.py
+++ run.py
@@ -16,7 +16,6 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix=COMMAND_PREFIX, intents=intents)
 
-speakers_data = fetch_json(SPEAKERS_ENDPOINT)
 
 if speakers_data:
     logging.info(speakers_data)
@@ -41,28 +40,7 @@ async def join(ctx):
 
 @bot.command(name="ls")
 async def list_styles(ctx):
-    """スピーカーとそのスタイルIDを表示します。"""
-    if not speakers_data:
-        await ctx.send("スピーカーのデータを取得できませんでした。")
-        return
 
-    # メッセージを整形して作成
-    message = "**利用可能なスピーカーとスタイル:**\n"
-    for speaker in speakers_data:
-        name = speaker["name"]
-        character_id = CHARACTORS_INFO.get(name, "unknown")  # キャラクターIDを取得
-        url = f"https://voicevox.hiroshiba.jp/dormitory/{character_id}/"
-        styles = ", ".join(
-            [f"{style['name']} (ID: {style['id']})" for style in speaker["styles"]]
-        )
-        message += f"\n[{name}]({url}): {styles}"
-
-    # メッセージの長さが2000文字を超えないように調整
-    if len(message) > 2000:
-        await ctx.send(message[:2000])
-        await ctx.send(message[2000:])
-    else:
-        await ctx.send(message)
 
 
 bot.run(os.getenv("VOICECHATLOIDTEST_TOKEN"))
