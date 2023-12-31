05ac456dd3edef496cc15aef1149382a53d71ddc
kuuchan-code <kuuchanxn@gmail.com>
2023-12-31 14:07:35 +0900

    update

--- utils.py
+++ utils.py
@@ -4,7 +4,6 @@ import json
 
 
 def fetch_character_infos(url):
-    # URLからデータを取得
     response = requests.get(url)
     if response.status_code == 200:
         data = response.text
@@ -14,15 +13,18 @@ def fetch_character_infos(url):
             r"export const characterInfos:.*?=\s*(\{.*?\n\})", data, re.DOTALL
         )
         if match:
-            # JavaScriptオブジェクトをPython辞書に変換
             character_infos_js = match.group(1)
-            # JavaScriptのオブジェクト表記をJSON形式に変換
-            character_infos_json = character_infos_js.replace("name", '"name"').replace(
-                "id", '"id"'
-            )
-            # JSONをPythonの辞書に変換
-            character_infos = json.loads(character_infos_json)
-            return character_infos
+
+            # JavaScriptのオブジェクトキーをJSONのキーに変換する（二重引用符で囲む）
+            character_infos_json = re.sub(r"(\w+):", r'"\1":', character_infos_js)
+
+            try:
+                # JSONをPythonの辞書に変換
+                character_infos = json.loads(character_infos_json)
+                return character_infos
+            except json.JSONDecodeError as e:
+                print(f"JSONデコードエラー: {e}")
+                return None
     else:
         print("データを取得できませんでした。ステータスコード:", response.status_code)
         return None
