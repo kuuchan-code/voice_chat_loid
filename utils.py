49c1fccee99a9c6f98b89ca32acb0b1e4e86f3cb
kuuchan-code <kuuchanxn@gmail.com>
2023-12-31 14:10:52 +0900

    update

--- utils.py
+++ utils.py
@@ -13,19 +13,18 @@ def fetch_character_infos(url):
         if match:
             character_infos_js = match.group(1)
 
-            # より堅牢な方法でキーを二重引用符で囲む
-            character_infos_json = re.sub(r'([\'"])?([a-zA-Z0-9_]+)([\'"])?:', r'"\2":', character_infos_js)
+            # すべてのキーを二重引用符で囲む
+            character_infos_json = re.sub(r'(?<!")(\w+)(?!"):', r'"\1":', character_infos_js)
 
-            # 不要な行末のカンマを削除（JavaScriptでは許容されるが、JSONでは許容されない）
-            character_infos_json = re.sub(r',\s*}', '}', character_infos_json)
-            character_infos_json = re.sub(r',\s*\]', ']', character_infos_json)
+            # 不要なカンマを削除
+            character_infos_json = re.sub(r',\s*([}\]])', r'\1', character_infos_json)
 
             try:
-                # JSONをPythonの辞書に変換
                 character_infos = json.loads(character_infos_json)
                 return character_infos
             except json.JSONDecodeError as e:
                 print(f"JSONデコードエラー: {e}")
+                print(character_infos_json)  # エラーが発生したJSONを出力
                 return None
     else:
         print("データを取得できませんでした。ステータスコード:", response.status_code)
