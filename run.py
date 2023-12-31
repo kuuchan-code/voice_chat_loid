b604b53fdc6ab28faccb73a781e716294804fc0f
kuuchan-code <kuuchanxn@gmail.com>
2023-12-31 15:01:29 +0900

    update

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from bot_commands import setup_commands
 from settings import (
     COMMAND_PREFIX,
+    TEST_GUILD_ID,
 )
 
 logging.basicConfig(level=logging.INFO)
@@ -18,6 +19,7 @@ bot = commands.Bot(intents=intents, command_prefix=COMMAND_PREFIX)
 @bot.event
 async def on_ready():
     logging.info(f"ログインしました。ユーザー名: {bot.user.name}!")
+    await bot.tree.sync(guild=TEST_GUILD_ID)
     setup_commands(bot)
 
 bot.run(os.getenv("VOICECHATLOIDTEST_TOKEN"))
