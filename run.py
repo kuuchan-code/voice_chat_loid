2b1a6a8493ee58f1f28ed2dd8fd4f3dc0b29c97e
kuuchan-code <kuuchanxn@gmail.com>
2023-12-31 15:41:38 +0900

    update

--- run.py
+++ run.py
@@ -36,7 +36,7 @@ async def list(ctx):
         styles = ", ".join(
             [f"{style['name']} (ID: {style['id']})" for style in speaker["styles"]]
         )
-        message += f"\n[{name[1:-1]}]({url}): {styles}"
+        message += f"\n[{name}]({url}): {styles}"
 
     # メッセージの長さが2000文字を超えないように調整
     if len(message) > 2000:
