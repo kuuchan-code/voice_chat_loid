9dd5f93b69d67388b618de8485b7e0cb635f2e17
kuuchan-code <kuuchanxn@gmail.com>
2023-12-31 13:27:49 +0900

    update

---
+++ voicevox_client.py
@@ -0,0 +1,10 @@
+import requests
+
+def fetch_json(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # ステータスコードが200以外の場合は例外を発生させる
+        return response.json()  # JSONデータをPythonの辞書として返す
+    except requests.RequestException as e:
+        print(f"リクエスト中にエラーが発生しました: {e}")
+        return None
