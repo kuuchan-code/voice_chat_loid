3cc43bf825af7e1bfd42a4dfab319906dd0b9072
kuuchan-code <kuuchanxn@gmail.com>
2023-12-31 16:03:17 +0900

    update

--- run.py
+++ run.py
@@ -31,26 +31,34 @@ async def list(ctx):
     message = "**利用可能なスピーカーとスタイル:**\n"
     for speaker in speakers_data:
         name = speaker["name"]
-        if name == "ナースロボ＿タイプＴ":
-            continue
-        elif name == "†聖騎士 紅桜†":
-            continue
-        else:
-            character_id = CHARACTORS_INFO.get(name, "unknown")  # キャラクターIDを取得
-            url = f"https://voicevox.hiroshiba.jp/dormitory/{character_id}/"
-            styles = ", ".join(
-                [f"{style['name']} (ID: {style['id']})" for style in speaker["styles"]]
-            )
-            message += f"\n[{name}]({url}): {styles}"
+        character_id = CHARACTORS_INFO.get(name, "unknown")  # キャラクターIDを取得
+        url = f"https://voicevox.hiroshiba.jp/dormitory/{character_id}/"
+        styles = ", ".join(
+            [f"{style['name']} (ID: {style['id']})" for style in speaker["styles"]]
+        )
+        message += f"\n[{name}]({url}): {styles}"
 
-    # メッセージの長さが2000文字を超えないように調整
-    # if len(message) > 2000:
-    #     await ctx.send(message[:2000])
-    #     await ctx.send(message[2000:])
-    # else:
-    await ctx.send(message)
+    # 長いメッセージを適切に分割して送信
+    await send_long_message(ctx, message)
 
-
+async def send_long_message(ctx, message, split_char='\n'):
+    """2000文字を超える長いメッセージを適切に分割して送信します。"""
+    while len(message) > 0:
+        # メッセージが2000文字以下の場合はそのまま送信
+        if len(message) <= 2000:
+            await ctx.send(message)
+            break
+        # メッセージを2000文字で仮に切り分け
+        part = message[:2000]
+        # 最後の改行位置または分割文字の位置を探す
+        split_pos = part.rfind(split_char)
+        if split_pos == -1:
+            # 分割文字が見つからない場合は、2000文字で強制的に分割
+            split_pos = 1999
+        # 最初の部分を送信
+        await ctx.send(message[:split_pos + 1])
+        # 残りのメッセージを更新
+        message = message[split_pos + 1:]
 @bot.command(name="join")
 async def join(ctx):
     if ctx.author.voice:
