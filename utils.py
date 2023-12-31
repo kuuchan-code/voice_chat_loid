a2671d2227a6375508b9b6b07a8282fc3b1f7f41
kuuchan-code <kuuchanxn@gmail.com>
2023-12-31 14:09:40 +0900

    update

--- utils.py
+++ utils.py
@@ -9,14 +9,16 @@ def fetch_character_infos(url):
         data = response.text
 
         # characterInfos変数の内容を抽出
-        match = re.search(
-            r"export const characterInfos:.*?=\s*(\{.*?\n\})", data, re.DOTALL
-        )
+        match = re.search(r"export const characterInfos:.*?=\s*(\{.*?\n\})", data, re.DOTALL)
         if match:
             character_infos_js = match.group(1)
 
-            # JavaScriptのオブジェクトキーをJSONのキーに変換する（二重引用符で囲む）
-            character_infos_json = re.sub(r"(\w+):", r'"\1":', character_infos_js)
+            # より堅牢な方法でキーを二重引用符で囲む
+            character_infos_json = re.sub(r'([\'"])?([a-zA-Z0-9_]+)([\'"])?:', r'"\2":', character_infos_js)
+
+            # 不要な行末のカンマを削除（JavaScriptでは許容されるが、JSONでは許容されない）
+            character_infos_json = re.sub(r',\s*}', '}', character_infos_json)
+            character_infos_json = re.sub(r',\s*\]', ']', character_infos_json)
 
             try:
                 # JSONをPythonの辞書に変換
