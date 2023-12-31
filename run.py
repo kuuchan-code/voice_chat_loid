9dd7d8116fd1939d298c59a6a7491f88bb10f628
kuuchan-code <kuuchanxn@gmail.com>
2023-12-31 13:10:09 +0900

    update

--- run.py
+++ run.py
@@ -1,19 +1,23 @@
+import os
 import discord
 
 intents = discord.Intents.default()
 
 client = discord.Client(intents=intents)
 
+
 @client.event
 async def on_ready():
-    print(f'ログインしました。ユーザー名: {client.user.name}!')
+    print(f"ログインしました。ユーザー名: {client.user.name}!")
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
-        await message.channel.send('こんにちは!')
+    if message.content.startswith("!hello"):
+        await message.channel.send("こんにちは!")
 
-client.run('YOUR_TOKEN')
+
+client.run(os.getenv("VOICECHATLOID_TOKEN"))
