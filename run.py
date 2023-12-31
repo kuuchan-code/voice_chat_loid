beecfdf48dadeef21b02090f554d59eb2856daf6
kuuchan-code <kuuchanxn@gmail.com>
2023-12-31 14:58:11 +0900

    update

--- run.py
+++ run.py
@@ -4,9 +4,7 @@ import logging
 from discord.ext import commands
 from bot_commands import setup_commands
 from settings import (
-    CHARACTORS_INFO,
     COMMAND_PREFIX,
-    SPEAKERS_ENDPOINT,
 )
 from voicevox_client import fetch_json
 
@@ -15,13 +13,12 @@ logging.basicConfig(level=logging.INFO)
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(intents=intents, command_prefix=COMMAND_PREFIX)
 
 
-@client.event
+@bot.event
 async def on_ready():
-    logging.info(f"ログインしました。ユーザー名: {client.user.name}!")
-    setup_commands(client)
+    logging.info(f"ログインしました。ユーザー名: {bot.user.name}!")
+    setup_commands(bot)
 
-
-client.run(os.getenv("VOICECHATLOIDTEST_TOKEN"))
+bot.run(os.getenv("VOICECHATLOIDTEST_TOKEN"))
