8df0ae3cc349468d9a6e07fb53dbf8970b5d544c
kuuchan-code <kuuchanxn@gmail.com>
2023-12-31 15:19:58 +0900

    update

--- run.py
+++ run.py
@@ -2,15 +2,16 @@ import os
 import discord
 from bot_commands import setup_commands
 
-from settings import COMMAND_PREFIX
+from settings import CHARACTORS_INFO, COMMAND_PREFIX, SPEAKERS_ENDPOINT
 from discord.ext import commands
+
+from voicevox_client import fetch_json
 
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(intents=intents, command_prefix=COMMAND_PREFIX)
-setup_commands(bot)
 
 
 @bot.event
@@ -22,5 +23,41 @@ async def on_ready():
 async def on_message(message):
     if message.author == bot.user:
         return
+    
+speakers_data = fetch_json(SPEAKERS_ENDPOINT)
+
+@bot.command(name="list", help="スピーカーとそのスタイルIDを表示します。")
+async def list(ctx):
+    """スピーカーとそのスタイルIDを表示します。"""
+    if not speakers_data:
+        await ctx.send("スピーカーのデータを取得できませんでした。")
+        return
+
+    # メッセージを整形して作成
+    message = "**利用可能なスピーカーとスタイル:**\n"
+    for speaker in speakers_data:
+        name = speaker["name"]
+        character_id = CHARACTORS_INFO.get(name, "unknown")  # キャラクターIDを取得
+        url = f"https://voicevox.hiroshiba.jp/dormitory/{character_id}/"
+        styles = ", ".join(
+            [f"{style['name']} (ID: {style['id']})" for style in speaker["styles"]]
+        )
+        message += f"\n[{name}]({url}): {styles}"
+
+    # メッセージの長さが2000文字を超えないように調整
+    if len(message) > 2000:
+        await ctx.send(message[:2000])
+        await ctx.send(message[2000:])
+    else:
+        await ctx.send(message)
+
+@bot.command(name="join")
+async def join(ctx):
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+        await channel.connect()
+        await ctx.send(f"{channel.name}に接続しました。")
+    else:
+        await ctx.send("あなたはボイスチャンネルにいません。")
 
 bot.run(os.getenv("VOICECHATLOIDTEST_TOKEN"))
